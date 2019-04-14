678010a33491147c0138d056cd1f84cbf01bc35d
LeonUBubble <1043065350@qq.com>
2019-04-13 21:36:39 -0400

    model test

--- webserver_legacy/app/__init__.py
+++ webserver_legacy/app/__init__.py
@@ -15,7 +15,7 @@ def create_app(config_name):
 	app = Flask(__name__, instance_relative_config=True)
 	app.config.from_object(app_config[config_name])
 	app.config.from_pyfile("config.py")
-	app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://<hostname>:<password>@localhost/bookyard"
+	app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:1234@localhost/bookyard"
 	app.secret_key = "secret_key"
 
 	login_manager.init_app(app)
